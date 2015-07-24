6bbbbc46213351a31e45300eb1d2df0c1712a7bc
gracevivar <grace_vb@hotmail.com>
2015-07-23 19:02:33 -0500

    prueba1

--- index.php
+++ index.php
@@ -1,43 +1,12 @@
-        <?php
+  <?php
         error_reporting(0);
 //         $url = parse_url(getenv("CLEARDB_DATABASE_URL "));
         $server = "us-cdbr-iron-east-02.cleardb.net";
-        $username = "be505b883b85da";
-        $password = "b22b5198";
-        $db = substr("heroku_a6c93f827357fde", 1);
+        $username = "b67c482eab4854";
+        $password = "c79d499e";
+        $db = "heroku_5da25e9f877d6fc";
         $conn = new mysqli($server, $username, $password, $db);
-
-
-        if ($conn->connect_error)
-            die($conn->connect_error);
-        $query = 'SELECT * FROM clasic';
-        $result = $conn->query($query);
-        if (!$result)
-            die($conn->error);
-        $num_rows = $result->num_rows;
-
-        echo '<meta charset="UTF-8">';
-        echo '<center>';
-        echo'<table>';
-        echo'<tr>';
-        echo '<th>' . 'autor' . '</th>' . '<th>' . 'titulo' . '</th>' . '<th>' . 'tipo' .
-        '</th>' . '<th>' . 'AÑO' . '</th>';
-        echo'</tr>';
-        for ($j = 0; $j < $num_rows; ++$j) {
-            //coger la siguiente fila
-            $result->data_seek($j);
-            $row = $result->fetch_array(MYSQLI_ASSOC);
-            echo'<tr>';
-            echo '<td>' . $row['autor'] . '</td>';
-            echo '<td>' . $row['titulo'] . '</td>';
-            echo '<td>' . $row['tipo'] . '</td>';
-            echo '<td>' . $row['anio'] . '</td>';
-            echo '</tr>';
-        }
-        echo'</table>';
-        echo '</center>';
-//CERRAR CONEXIONES
-        $result->close();
-        $conn->close();
-        ?>
-
+		if ($conn->connect_error)
+		    die($conn->connect_error);
+		else
+		   echo 'Conexion exitosa' . '<br>' . '<br>';
