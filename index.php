03ca569b27a4e5745be0db77a5f463f699e9556e
gracevivar <grace_vb@hotmail.com>
2015-07-25 16:58:03 -0500

    prueba

--- index.php
+++ index.php
@@ -130,7 +130,7 @@ else
         <div class="container marketing">
 
             <!-- Three columns of text below the carousel -->
-            <div class="row">
+            <div class="row let">
                 <div class="col-lg-4">
                     <img class="img-circle" src="img/c1.jpg" alt="Generic placeholder image" width="140" height="140">
                     <h2>Nuetra farmacia </h2>
@@ -148,40 +148,29 @@ else
                     <p>Ven y disfruta de nuestros servicio de internet y calidad con nosotros </p>
                 </div><!-- /.col-lg-4 -->
             </div><!-- /.row -->
-
-
-            <!-- START THE FEATURETTES -->
-
             <hr class="featurette-divider">
 
-            <div class="row featurette">
+            <div class="row featurette let">
                 <div class="col-md-7">
                     <h2 class="featurette-heading">Productos</h2>
                     <p class="lead">Los mejores precios
                         <br>Digestivos, Aalgesicos, vitaminas</p>
                 </div>
-                <div class="col-md-5">
-                    <img class="featurette-image img-responsive center-block" data-src="holder.js/500x500/auto" alt="Generic placeholder image">
-                </div>
             </div>
-
             <hr class="featurette-divider">
 
-            <div class="row featurette">
+            <div class="row featurette let">
                 <div class="col-md-7">
                     <h2 class="featurette-heading">La mejor calidad</h2>
                     <p class="lead">La satisfacion del cliente es lo importante</p>
                 </div>
             </div>
-
-            <hr class="featurette-divider">
+            <hr class="featurette-divider let">
             <footer>
                 <p class="pull-right"><a href="https://www.facebook.com/asi.esfotepn?fref=ts">ASI-EPN</a></p>
                 <p>Farmaxis. · <a href="http://esfot.epn.edu.ec/esfot30/">ESFOT</a> · <a href="http://www.epn.edu.ec/">EPN</a></p>
             </footer>
-
         </div><!-- /.container -->
-
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
         <script src="js/bootstrap.js"></script>
         <script src="./Carousel Template for Bootstrap_files/ie10-viewport-bug-workaround.js"></script>
